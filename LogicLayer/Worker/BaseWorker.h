568d5032915af4f2405a4a9b2e434c8fdcf45864
Alexey Gavrilov <alexgavrilov28@gmail.com>
2020-12-19 15:30:08 +0300

    Initial commit

---
+++ LogicLayer/Worker/BaseWorker.h
@@ -0,0 +1,17 @@
+//
+// Created by Alexey on 19.12.2020.
+//
+
+#ifndef REPORTS_BASEWORKER_H
+#define REPORTS_BASEWORKER_H
+
+#include <string>
+using namespace std;
+
+class BaseWorker {
+    string Name;
+
+};
+
+
+#endif //REPORTS_BASEWORKER_H
---
+++ LogicLayer/Worker/CompanyWorkers.h
@@ -0,0 +1,14 @@
+//
+// Created by Alexey on 19.12.2020.
+//
+
+#ifndef REPORTS_COMPANYWORKERS_H
+#define REPORTS_COMPANYWORKERS_H
+
+
+class CompanyWorkers {
+
+};
+
+
+#endif //REPORTS_COMPANYWORKERS_H
